373c3cbb36db0bcba0386f40ae951eeba70fd0e1
Lalo <eduardo.j.a@ciencias.unam.mx>
2021-09-30 09:21:04 -0500

    Change image and add dat.gui

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 import * as THREE from 'three';
-import img from './images/img01.jpg';
+import img from './images/img02.png';
 import noise from './images/noise1.jpg';
+import dat from 'dat.gui';
 
 const textureLoader = new THREE.TextureLoader();
 const imageTexture = textureLoader.load(img);
 imageTexture.wrapS = THREE.MirroredRepeatWrapping;
 imageTexture.wrapT = THREE.MirroredRepeatWrapping;
 const noiseTexture = textureLoader.load(noise);
+noiseTexture.wrapS = THREE.MirroredRepeatWrapping;
+noiseTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+const imageWidth = 795;
+const imageHeight = 912;
+const imageAspect = 795/912;
+
+const debugObject = {
+  strength: 0.2,
+  speed: 0.5,
+  distortionScale: 2
+}
+const gui = new dat.GUI();
+
+gui.add(debugObject, 'strength', 0, 2, 0.01).onChange(() => {
+  uniforms.uStrength.value = debugObject.strength;
+});
+gui.add(debugObject, 'speed', 0, 2, 0.01).onChange(() => {
+  uniforms.uSpeed.value = debugObject.speed;
+});
+gui.add(debugObject, 'distortionScale', 0.3, 5, 0.01).onChange(() => {
+  uniforms.uDistortionScale.value = debugObject.distortionScale;
+});
+
 
 const container = document.querySelector('.webgl-container');
 
@@ -38,7 +63,11 @@ window.addEventListener('resize', resize);
 const uniforms = {
   uTime: { value: 0 },
   uNoise: {value: noiseTexture},
-  uTexture: {value: imageTexture}
+  uSpeed: {value: debugObject.speed},
+  uStrength: {value: debugObject.strength},
+  uTexture: {value: imageTexture},
+  uAmplitude: {value: debugObject.amplitude},
+  uDistortionScale: {value: debugObject.distortionScale}
 }
 
 const mesh = new THREE.Mesh(
@@ -56,6 +85,9 @@ const mesh = new THREE.Mesh(
       uniform float uTime;
       uniform sampler2D uTexture;
       uniform sampler2D uNoise;
+      uniform float uStrength;
+      uniform float uSpeed;
+      uniform float uDistortionScale;
       varying vec2 vUv;
 
       vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
@@ -88,12 +120,14 @@ const mesh = new THREE.Mesh(
       }
 
       void main() {
-        vec4 noise = texture2D(uNoise, vUv);
-        gl_FragColor = texture2D(uTexture, vUv + snoise(vUv * 2.0 + uTime * 0.5));
+        float noise = snoise(vUv * uDistortionScale + uTime * uSpeed);
+        gl_FragColor = texture2D(uTexture, vUv + noise * uStrength);
       }
     `
   })
 );
+
+mesh.scale.x = imageAspect;
 
 scene.add(mesh);
 
